5d97a6f6099c51d3ca45a1a22945e7b805265970
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-29 09:25:00 +0200

    Validate user the requested recipe´s id and render NotFound page when no recipe has been found

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+import type { RouteLocationNormalized } from "vue-router";
+import type { Recipe } from "./recipes";
+
 import RecipeDetails from "./pages/recipe/RecipeDetails.vue";
 import RecipeSearchPage from "./pages/recipe/RecipeSearchPage.vue";
 import NotFound from "./pages/NotFound.vue";
+
+import { recipes } from "./recipes";
 
 const routes = [
   {
@@ -17,6 +22,19 @@ const routes = [
     path: "/recipes/:id",
     component: RecipeDetails,
     props: true,
+    beforeEnter: (
+      to: RouteLocationNormalized,
+      _from: RouteLocationNormalized
+    ) => {
+      if (!recipeFound(recipes, to.params.id as string)) {
+        return {
+          name: "NotFound",
+          params: { pathMatch: to.path.split("/").slice(1) },
+          query: to.query,
+          hash: to.hash,
+        };
+      }
+    },
   },
   { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
@@ -25,3 +43,7 @@ export const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+function recipeFound(recipes: Recipe[], id: string): boolean {
+  return recipes.find((currRecipe: Recipe) => currRecipe.id === id) != null;
+}
